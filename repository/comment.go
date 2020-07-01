c7e9d654d8ae5f0eb0acd7f2ef352fabd2c6f126
OhMinSeop <mins5190@gmail.com>
2020-07-02 01:06:25 +0900

    [댓글] 댓글 서비스 코드 수정

--- repository/comment.go
+++ repository/comment.go
@@ -1,1 +1,145 @@
 package repository
+
+import (
+	"github.com/OhMinsSup/story-server/database/models"
+	"github.com/OhMinsSup/story-server/dto"
+	"github.com/OhMinsSup/story-server/helpers"
+	"github.com/jinzhu/gorm"
+)
+
+type CommentRepository struct {
+	db *gorm.DB
+}
+
+func NewCommentRepository(db *gorm.DB) *CommentRepository {
+	return &CommentRepository{
+		db: db,
+	}
+}
+
+func (c *CommentRepository) CreateComment(body dto.CommentParams, userId string) error {
+	var postData dto.PostRawQueryUserProfileResult
+	if err := c.db.Raw(`
+		SELECT p.*, u.id, u.username, u.email, up.display_name, up.thumbnail as user_thumbnail FROM "posts" AS p
+		LEFT OUTER JOIN "users" AS u ON u.id = p.user_id
+		LEFT OUTER JOIN "user_profiles" AS up ON up.user_id = u.id
+		WHERE p.id = ?`, body.PostId).Scan(&postData).Error; err != nil {
+		return err
+	}
+
+	tx := c.db.Begin()
+	var comment models.Comment
+
+	if body.CommentId != "" {
+		var commentTarget models.Comment
+		if err := tx.Where("id = ?", body.CommentId).First(&commentTarget).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+
+		comment.Level = commentTarget.Level + 1
+		comment.ReplyTo = body.CommentId
+
+		if commentTarget.Level > 3 {
+			tx.Rollback()
+			return helpers.ErrorMaxCommentLevel
+		}
+
+		commentTarget.HasReplies = true
+		if err := tx.Model(&commentTarget).Updates(map[string]interface{}{
+			"has_replies": true,
+		}).Error; err != nil {
+			tx.Rollback()
+			return err
+		}
+	}
+
+	comment.Text = body.Text
+	comment.PostId = body.PostId
+	comment.UserId = userId
+
+	if err := tx.Create(&comment).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	newPostScore := models.PostScore{
+		Type:   "COMMENT",
+		PostId: body.PostId,
+		UserId: userId,
+		Score:  2.5,
+	}
+
+	if err := tx.Create(&newPostScore).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+func (c *CommentRepository) UpdateComment(body dto.CommentParams, userId string) error {
+	tx := c.db.Begin()
+
+	var comment models.Comment
+	if err := tx.Where("id = ?", body.CommentId).First(&comment).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	if userId != comment.UserId {
+		tx.Rollback()
+		return helpers.ErrorPermission
+	}
+
+	if err := tx.Model(&comment).Updates(map[string]interface{}{
+		"text": body.Text,
+	}).Error; err != nil {
+		tx.Rollback()
+		return err
+	}
+
+	return tx.Commit().Error
+}
+
+func (c *CommentRepository) DeleteComment(body dto.CommentParams, userId string) error {
+	var comment models.Comment
+	if err := c.db.Where("id = ?", body.CommentId).First(&comment).Error; err != nil {
+		return err
+	}
+
+	if userId != comment.UserId {
+		return helpers.ErrorPermission
+	}
+
+	var postData dto.PostRawQueryUserProfileResult
+	if err := c.db.Raw(`
+		SELECT p.*, u.id, u.username, u.email, up.display_name FROM "posts" AS p
+		LEFT OUTER JOIN "users" AS u ON u.id = p.user_id
+		LEFT OUTER JOIN "user_profiles" AS up ON up.user_id = u.id
+		WHERE p.id = ?`, body.PostId).Scan(&postData).Error; err != nil {
+		return err
+	}
+
+	tx := c.db.Begin()
+
+	if err := tx.Model(&comment).Updates(map[string]interface{}{
+		"deleted": true,
+	}).Error; err != nil {
+		return err
+	}
+
+	var score models.PostScore
+	if err := c.db.Raw(`
+		SELECT * FROM "post_scores" AS ps 
+		WHERE ps.post_id = ?
+		AND ps.user_id = ?
+		AND ps.type = 'COMMENT'
+		ORDER BY ps.created_at DESC
+	`, body.PostId, userId).Scan(&score).Error; err != nil {
+		return err
+	}
+
+	tx.Delete(&score)
+	return tx.Commit().Error
+}
--- services/comment.go
+++ services/comment.go
@@ -2,9 +2,9 @@ package services
 
 import (
 	"fmt"
-	"github.com/OhMinsSup/story-server/database/models"
 	"github.com/OhMinsSup/story-server/dto"
 	"github.com/OhMinsSup/story-server/helpers"
+	"github.com/OhMinsSup/story-server/repository"
 	"github.com/gin-gonic/gin"
 	"github.com/jinzhu/gorm"
 	"net/http"
@@ -12,120 +12,34 @@ import (
 
 func WriteCommentService(body dto.CommentParams, db *gorm.DB, ctx *gin.Context) (helpers.JSON, int, error) {
 	userId := fmt.Sprintf("%v", ctx.MustGet("id"))
-	var postData dto.PostRawQueryUserProfileResult
-	if err := db.Raw(`
-		SELECT p.*, u.id, u.username, u.email, up.display_name, up.thumbnail as user_thumbnail FROM "posts" AS p
-		LEFT OUTER JOIN "users" AS u ON u.id = p.user_id
-		LEFT OUTER JOIN "user_profiles" AS up ON up.user_id = u.id
-		WHERE p.id = ?`, body.PostId).Scan(&postData).Error; err != nil {
-		return nil, http.StatusNotFound, err
+	commentRepository := repository.NewCommentRepository(db)
+	if err := commentRepository.CreateComment(body, userId); err != nil {
+		return nil, http.StatusInternalServerError, err
 	}
-
-	var comment models.Comment
-
-	if body.CommentId != "" {
-		var commentTarget models.Comment
-		if err := db.Where("id = ?", body.CommentId).First(&commentTarget).Error; err != nil {
-			return nil, http.StatusNotFound, err
-		}
-
-		comment.Level = commentTarget.Level + 1
-		comment.ReplyTo = body.CommentId
-
-		if commentTarget.Level > 3 {
-			return nil, http.StatusBadRequest, helpers.ErrorMaxCommentLevel
-		}
-
-		commentTarget.HasReplies = true
-		if err := db.Model(&commentTarget).Update(map[string]interface{}{
-			"has_replies": true,
-		}).Error; err != nil {
-			return nil, http.StatusNotModified, err
-		}
-	}
-
-	comment.Text = body.Text
-	comment.PostId = body.PostId
-	comment.UserId = userId
-
-	db.NewRecord(comment)
-	db.Create(&comment)
-
-	newPostScore := models.PostScore{
-		Type:   "COMMENT",
-		PostId: body.PostId,
-		UserId: userId,
-		Score:  2.5,
-	}
-
-	db.NewRecord(newPostScore)
-	db.Create(&newPostScore)
 
 	return helpers.JSON{
-		"comment": comment.Serialize(),
+		"comment": true,
 	}, http.StatusOK, nil
 }
 
 func EditCommentService(body dto.CommentParams, db *gorm.DB, ctx *gin.Context) (helpers.JSON, int, error) {
 	userId := fmt.Sprintf("%v", ctx.MustGet("id"))
-	var comment models.Comment
-	if err := db.Where("id = ?", body.CommentId).First(&comment).Error; err != nil {
-		return nil, http.StatusNotFound, err
-	}
-
-	if userId != comment.UserId {
-		return nil, http.StatusUnauthorized, helpers.ErrorPermission
-	}
-
-	if err := db.Model(&comment).Update(map[string]interface{}{
-		"text": body.Text,
-	}).Error; err != nil {
-		return nil, http.StatusNotModified, err
+	commentRepository := repository.NewCommentRepository(db)
+	if err := commentRepository.UpdateComment(body, userId); err != nil {
+		return nil, http.StatusInternalServerError, err
 	}
 
 	return helpers.JSON{
-		"comment": comment.Serialize(),
+		"comment": true,
 	}, http.StatusOK, nil
 }
 
 func RemoveCommentService(body dto.CommentParams, db *gorm.DB, ctx *gin.Context) (helpers.JSON, int, error) {
 	userId := fmt.Sprintf("%v", ctx.MustGet("id"))
-	var comment models.Comment
-	if err := db.Where("id = ?", body.CommentId).First(&comment).Error; err != nil {
-		return nil, http.StatusNotFound, err
+	commentRepository := repository.NewCommentRepository(db)
+	if err := commentRepository.DeleteComment(body, userId); err != nil {
+		return nil, http.StatusInternalServerError, err
 	}
-
-	if userId != comment.UserId {
-		return nil, http.StatusUnauthorized, helpers.ErrorPermission
-	}
-
-	var postData dto.PostRawQueryUserProfileResult
-	if err := db.Raw(`
-		SELECT p.*, u.id, u.username, u.email, up.display_name FROM "posts" AS p
-		LEFT OUTER JOIN "users" AS u ON u.id = p.user_id
-		LEFT OUTER JOIN "user_profiles" AS up ON up.user_id = u.id
-		WHERE p.id = ?`, body.PostId).Scan(&postData).Error; err != nil {
-		return nil, http.StatusNotFound, err
-	}
-
-	if err := db.Model(&comment).Update(map[string]interface{}{
-		"deleted": true,
-	}).Error; err != nil {
-		return nil, http.StatusNotModified, err
-	}
-
-	var score models.PostScore
-	if err := db.Raw(`
-		SELECT * FROM "post_scores" AS ps 
-		WHERE ps.post_id = ?
-		AND ps.user_id = ?
-		AND ps.type = 'COMMENT'
-		ORDER BY ps.created_at DESC
-	`, body.PostId, userId).Scan(&score).Error; err != nil {
-		return nil, http.StatusNotFound, err
-	}
-
-	db.Delete(&score)
 
 	return helpers.JSON{
 		"comment": true,
