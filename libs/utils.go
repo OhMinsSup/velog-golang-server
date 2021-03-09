4ca44d66c3fef94226a70a0569a32d1ba568d684
OhMinSeop <mins5190@gmail.com>
2021-03-09 22:14:08 +0900

    ADD 포스트 작성 (2)

--- libs/utils.go
+++ libs/utils.go
@@ -2,10 +2,7 @@ package libs
 
 import (
 	"crypto/sha256"
-	"database/sql"
 	"encoding/hex"
-	"github.com/OhMinsSup/story-server/ent"
-	"github.com/gin-gonic/gin"
 	"os"
 )
 
@@ -25,31 +22,4 @@ func CreateHash(data string) string {
 // GetEnvWithKey process.env 에 key값과 일치하는 값을 가져온다
 func GetEnvWithKey(key string) string {
 	return os.Getenv(key)
-}
-
-// Repository database client
-type Repository struct {
-	Client *ent.Client
-	DB     *sql.DB
-}
-
-// NewRepository - Repository client Create
-func NewRepository(ctx *gin.Context) *Repository {
-	client := ctx.MustGet("client").(*ent.Client)
-	db := ctx.MustGet("db").(*sql.DB)
-
-	return &Repository{
-		Client: client,
-		DB:     db,
-	}
-}
-
-// GetClient - get entGo client
-func (repository *Repository) GetClient() *ent.Client {
-	return repository.Client
-}
-
-// GetClient - get sql client
-func (repository *Repository) GetDB() *sql.DB {
-	return repository.DB
 }
